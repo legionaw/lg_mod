1c52d079a02f1b3d184ae1735f691ddbe604d94e
The Duke of Rockford <jonsavel@gmail.com>
2023-11-18 10:27:48 -0600

    Initial commit

---
+++ common/defines/lg_defines.lua
@@ -0,0 +1,49 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+--GAME--
+NDefines.NGame.END_DATE = "1836.12.31"																--
+
+--DIPLOMACY--
+NDefines.NDiplomacy.IMPERIAL_REFORM_COST = 100												--
+NDefines.NDiplomacy.PO_REVOKE_REFORM_PEACE_COST = 50									--
+NDefines.NDiplomacy.PEACE_COST_CHANGE_HRE_RELIGION = 300							--
+
+--COUNTRY--
+--NDefines.NCountry.ALLOW_ESTATE_AND_PARLIAMENT_SEAT_IN_PROVINCE = 1	-- Whether or not a province can have both an estate and a parliament seat <-- OBSOLETE
+NDefines.NCountry.FREE_IDEA_GROUP_COST = 7														--
+NDefines.NCountry.PS_BUY_IDEA = 250																		--
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 7															--
+NDefines.NCountry.MAX_EXTRA_PERSONALITIES = 4													-- Number of personalities that can be gained except the one you get when turning 15.
+NDefines.NCountry.NUM_PARLIAMENT_ISSUES = 7														--
+NDefines.NCountry.CAN_CONVERT_TERRITORY_CULTURE = 1										-- Defines if you are allowed to convert religion of territory provinces
+NDefines.NCountry.CAN_CONVERT_TERRITORY_RELIGION = 1									-- Defines if you are allowed to convert religion of territory provinces
+
+--ECONOMY--
+NDefines.NEconomy.GOLD_MINE_DEPLETION_THRESHOLD = 10									-- Gold mines above production level or above can be depleted
+
+--MILITARY--
+--NDefines.NMilitary.MARINE_SHOCK_DAMAGE_TAKEN = 0.25									--
+
+--RELIGION--
+NDefines.NReligion.MAX_CHURCH_POWER = 600															--
+
+--SUSPENDED--
+--NDefines.NCountry.MISSION_CANCEL_CHOOSE_NEXT_DELAY = 0							--
+--NDefines.NCountry.COLONIAL_NATION_GROWTH_IMPACT = 1									-- Colonies of colonial nations grow slower if desired.
+--NDefines.NCountry.PARLIAMENT_ISSUE_DURATION = 0.5										-- In years
+--NDefines.NCountry.PARLIAMENT_DEBATE_DURATION = 0.5									-- In years
+--NDefines.NCountry.PS_CHANGE_CULTURE = 20														--
+--NDefines.NCountry.OVERSEAS_CLIENT_STATES = 1												-- allow overseas client states?
+--NDefines.NCountry.PRIMARY_NATION_BLOCK_CONVERT_CULTURE = 0					-- Does primary nation of a tag existing block converting culture?
+--NDefines.NCountry.POWER_MAX = 999																		-- How much power can be stored at maximum.
+--NDefines.NDiplomacy.INFILTRATE_ADMINISTRATION_COST = 50							--
+--NDefines.NDiplomacy.INFILTRATE_ADMINISTRATION_DURATION = 8					--
+--NDefines.NDiplomacy.NUM_POSSIBLE_RIVALS = 4													--
+--NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.03								--
+--NDefines.NGame.GREAT_POWER_FRENZY_QUICKSTART = 1										--
+--NDefines.NMilitary.INF_REGIMENT_MANPOWER_COST = 1.0									-- 1 = 1000 men in cost.
+--NDefines.NMilitary.CAV_REGIMENT_MANPOWER_COST = 1.0									-- 1 = 1000 men in cost.
+--NDefines.NMilitary.ART_REGIMENT_MANPOWER_COST = 1.0									-- 1 = 1000 men in cost.
+--NDefines.NReligion.MAX_UNLOCKED_ASPECTS = 7													--
